db622c4fc654023b2b096444d42cdeebee76606f
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-03-25 09:07:19 +0200

    First separated

---
+++ HowToreateRepository/Function.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+
+const int ROWS = 10;
+const int COLS = 10;
+
+void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
+void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS);
+
+void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
+void Print(double arr[ROWS][COLS], const int ROWS, const int COLS);
+
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS);
+void Sort(double arr[ROWS][COLS], const int ROWS, const int COLS);
