988a3be5f959c34ca3fa396fc739d03fe3bd4845
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-03-25 09:28:02 +0200

    First commit

--- HowToreateRepository/Function.h
+++ HowToreateRepository/Function.h
@@ -4,17 +4,11 @@
 const int ROWS = 10;
 const int COLS = 10;
 
-void FillRand(int arr[], const int n);
-void FillRand(double arr[], const int n);
-void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
-void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T>void FillRand(T arr[], const int n);
+template<typename T>void FillRand(T arr[ROWS][COLS], const int ROWS, const int COLS);
 
-void Print(int arr[], const int n);
-void Print(double arr[], const int n);
-void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
-void Print(double arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T>void Print(T arr[], const int n);
+template<typename T>void Print(T arr[ROWS][COLS], const int ROWS, const int COLS);
 
-void Sort(int arr[], const int n);
-void Sort(double arr[], const int n);
-void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS);
-void Sort(double arr[ROWS][COLS], const int ROWS, const int COLS);
+template<typename T>void Sort(T arr[], const int n);
+template<typename T>void Sort(T arr[ROWS][COLS], const int ROWS, const int COLS);
--- HowToreateRepository/main.cpp
+++ HowToreateRepository/main.cpp
@@ -1,4 +1,5 @@
 #include"Function.h"
+#include"Template.cpp"
 //Git Branches
 
 void main() {
