e8094a2ec811a02c58626acbcff6a1f8e6a625e0
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-03-27 22:34:15 +0200

    First commit

--- HowToreateRepository/Function.h
+++ HowToreateRepository/Function.h
@@ -5,5 +5,5 @@ const int FACTORIAL_MAX = 20;
 const int FACTORIAL_MIN = 0;
 
 template<typename T>T CalcFactorial(T factorial);
-
-template<typename T>void Print(T data);
+template<typename T>void elevator(T floor);
+template<typename T>void ResaltValue(T data);
--- HowToreateRepository/main.cpp
+++ HowToreateRepository/main.cpp
@@ -7,7 +7,7 @@ void main() {
 	setlocale(LC_ALL, "");
 
 	long long factorial;
-	Print("Для вычисления Факториала введите целое число от 0 до 20");
+	std::cout << "Для вычисления Факториала введите целое число от 0 до 20" << "\t";
 	std::cin >> factorial; std::cout << std::endl;
 	ResaltValue(factorial);
 	int floor = 5;
