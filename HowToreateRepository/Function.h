332d254d30fb98fabc52cc03577284b59b4d20f7
Alexandr-Opryshko <alexandr.opryshko.07@gmail.com>
2020-04-01 12:02:20 +0300

    First commit

--- HowToreateRepository/Function.h
+++ HowToreateRepository/Function.h
@@ -1,11 +1,4 @@
 #pragma once
 #include<iostream>
-#include<conio.h>
 
-const int X = 3;
-const int Y = 3;
-const int SIZE_COUNTER = X * Y;
 
-void Coordinates(int booton, char sumbol, char arr[Y][X]);
-void Grid(char arr[Y][X]);
-int EndGame(char arr[Y][X]);
--- HowToreateRepository/main.cpp
+++ HowToreateRepository/main.cpp
@@ -6,32 +6,5 @@
 
 
 void main() {
-	char sumbol;
-	int booton;
-	int counter = 0;
-	int temp;
-	char arrCh[Y][X] = {};
-	Grid(arrCh);
-	while (true) {
-		booton = _getch();
-		counter++ % 2 == 0 ? sumbol = 'X' : sumbol = 'O';
-		Coordinates(booton, sumbol, arrCh);
-		Grid(arrCh);
-		temp = EndGame(arrCh);
-		if (temp == false) {
-			setlocale(LC_ALL, "");
-			std::cout << "Ничья" << std::endl;
-			return;
-		}
-		else if (temp == 'X') {
-			setlocale(LC_ALL, "");
-			std::cout << "Победил участник 1 - X" << std::endl;
-			return;
-		}
-		else if (temp == 'O') {
-			setlocale(LC_ALL, "");
-			std::cout << "Победил участник 2 - O" << std::endl;
-			return;
-		}
-	}
+
 }
